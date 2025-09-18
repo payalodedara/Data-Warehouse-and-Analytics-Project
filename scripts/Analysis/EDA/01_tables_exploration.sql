fbe3dba9bf0ab855004f0691018a07065e9d3c32
Payal Odedara <127177932+payalodedara@users.noreply.github.com>
2025-09-18 22:46:11 +0530

    Create 01_tables_exploration.sql

---
+++ scripts/Analysis/EDA/01_tables_exploration.sql
@@ -0,0 +1,5 @@
+-- ---------------- Tables Exploration --------------------------
+
+select * from gold.dim_customer limit 10;
+select * from gold.dim_product limit 10;
+select * from gold.fact_sales limit 10;
