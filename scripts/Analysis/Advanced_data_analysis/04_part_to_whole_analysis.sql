2ba7a2a1e87c1b9f7ef71adaa5befcaacae1adb6
Payal Odedara <127177932+payalodedara@users.noreply.github.com>
2025-09-18 22:59:26 +0530

    Create 04_part_to_whole_analysis.sql

---
+++ scripts/Analysis/Advanced_data_analysis/04_part_to_whole_analysis.sql
@@ -0,0 +1,15 @@
+- --------- PART TO WHOLE ANALYSIS ------------------
+-- ==================================================
+
+-- which category contributes to the overasll sales
+
+with category_sales as(
+select p.category, sum(f.sales_amount) as total_sales
+from gold.fact_sales f
+left join gold.dim_product p on f.product_key = p.product_key
+group by p.category
+)
+
+select category, total_sales, sum(total_sales) over() overall_sales,
+(total_sales/sum(total_sales) over()) * 100 as per_of_total
+from category_sales;
